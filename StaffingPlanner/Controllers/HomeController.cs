bd535c80e2e66682bb7d57a353883ad931c881b1
mdng223 <mdng223@g.uky.edu>
2018-05-16 13:26:19 -0400

    Log in and logout works.

--- StaffingPlanner/Controllers/HomeController.cs
+++ StaffingPlanner/Controllers/HomeController.cs
@@ -13,24 +13,47 @@ namespace StaffingPlanner.Controllers
 	public class HomeController : Controller
 	{
         private DEV_ClientOpportunitiesEntities db = new DEV_ClientOpportunitiesEntities();
+
+
         public ActionResult Index()
+        {
+            System.Diagnostics.Debug.WriteLine("Get Index");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(SIGNUP_INFO objUser)
 		{
-			return View();
+            System.Diagnostics.Debug.WriteLine("Post Index");
+            if (!ModelState.IsValid)
+            {
+                System.Diagnostics.Debug.WriteLine("Model is valid");
+                using (db)
+                {
+                    System.Diagnostics.Debug.WriteLine("Using DB");
+                    var obj = db.SIGNUP_INFO.Where(a => a.EMAIL.Equals(objUser.EMAIL) && a.USER_PASSWORD.Equals(objUser.USER_PASSWORD)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        System.Diagnostics.Debug.WriteLine("User exists!");
+                        Session["UserID"] = obj.USER_ID.ToString();
+                        Session["FirstName"] = obj.FIRSTNAME.ToString();
+                        Session["Privilege"] = obj.USER_PRIVILEGE_ID.ToString();
+                        return RedirectToAction("/");
+
+                    }
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("User does not exist!");
+            return View(objUser);
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
+        public ActionResult Logout()
+        {
+            System.Diagnostics.Debug.WriteLine("Logging out! Abandoning cookies");
+            Session.Abandon();
+            return RedirectToAction("/");
+        }
 
         public ActionResult Report()
         {
