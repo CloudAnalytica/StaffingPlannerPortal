7efb2bff827ab05a675c38b800d6a998ce8993bf
Zoe Baker <Zoe.Baker@us.sogeti.com>
2018-06-08 10:05:35 -0400

    Changes spacing in comments to test Jenkins deployment shell scripts.

--- StaffingPlanner/Controllers/StatisticsController.cs
+++ StaffingPlanner/Controllers/StatisticsController.cs
@@ -1,5 +1,4 @@
 ﻿/*
- * 
  * Purpose: This controller handles calling the stored procedures in the entity framework 
  *			and sends the information to the view to be turned into a google chart. 
  */
